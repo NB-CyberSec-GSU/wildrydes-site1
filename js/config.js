0ac489dcf26bb761594acdc7adda9f6252836736
NB-CyberSec-GSU <nbarro1@gsu.edu>
2025-11-03 15:39:30 -0500

    Update config.js
    Added user pool ID and user pool client ID

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_UL7iswYgb', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '30103i2g7skjumet7q9a1dur2p', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
